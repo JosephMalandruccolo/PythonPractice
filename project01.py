009a277557d5fbb3a80ae77baaab763dc4859d42
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-01-13 23:21:31 -0600

    first commit

---
+++ project01.py
@@ -0,0 +1,35 @@
+#
+#	MSU CSE 231 Fall 2007 Project 1
+#	author: Joseph Malandruccolo
+#	date:	January 13, 2013
+#
+#	Program Specs:
+#		Prompt the user for two integers
+#		Display the result of each of the following:
+#			1) add the first and second numbers
+#			2) subtract the second number from the first number
+#			3) multiply the first and second numbers
+#			4) integer division between first and second numbers and remainder
+
+
+#prompt the user
+print 'Please enter two integers\nAddition, subtraction, multiplication, and division results will be returned'
+bValidInput = False
+while bValidInput == False:
+	try:
+		numStr = raw_input('Please enter the first integer')
+		nVar1 = int(numStr)
+		numStr = raw_input('Please enter the second integer')
+		nVar2 = int(numStr)
+		bValidInput = True
+	except:
+		print 'Invalid input: please enter two valid integers'
+
+#compute 1-4
+print "Addition: ",nVar1 + nVar2
+print "Subtraction: ",nVar1 - nVar2
+print "Multiplication: ", nVar1 * nVar2
+if nVar2 == 0:
+	print 'Division by 0 is undefined'
+else:
+	print "Division: ", nVar1 / nVar2, " remainder ", nVar1 % nVar2
---
+++ readme.txt
@@ -0,0 +1,2 @@
+A collection of projects I'm using to learn Python, courtesy of Michigan State University.
+http://www.cse.msu.edu/~cse231/PracticeOfComputingUsingPython/index.php
